f70b8b1522725e7edbc7f769a61084b6052ed6c7
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-28 19:39:40 -0300

    Implement create user feature

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { UserService } from '../../services/user/user.service';
+import { UserRequest } from '../../interfaces/User/UserRequest';
 
 @Component({
   selector: 'app-home',
@@ -18,7 +21,11 @@ export class HomeComponent {
     password: ['', Validators.required]
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private messageService: MessageService
+  ) { }
 
   onSubmitLoginForm() {
     console.warn(this.loginForm.value);
@@ -26,5 +33,23 @@ export class HomeComponent {
 
   onSubmitSignupForm(): void {
     console.warn(this.signUpForm.value)
+    if (this.signUpForm.value && this.signUpForm.valid) {
+      this.userService.signUpUser(this.signUpForm.value as UserRequest)
+        .subscribe({
+          next: (response) => {
+            this.messageService.add({
+              severity: 'success', summary: 'Sucesso', detail: 'Usuário criado com sucesso!'
+            })
+            this.signUpForm.reset();
+            this.loginCard = true;
+          },
+          error: (error) => {
+            console.log(error);
+            this.messageService.add({
+              severity: 'error', summary: 'Erro', detail: 'Erro ao criar usuário!'
+            })
+          }
+        })
+    }
   }
 }
