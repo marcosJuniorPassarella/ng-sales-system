43d8c0f685649c1973af2f7cc2ff1651f5bc4a11
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-28 19:47:12 -0300

    Implement auth user method

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserRequest } from '../../interfaces/User/UserRequest';
+import { AuthRequest } from '../../interfaces/User/AuthRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,13 @@ export class UserService {
   signUpUser(requestDatas: UserRequest): Observable<UserRequest> {
     return this.http.post<UserRequest>(
       `${this.API_URL}/user`,
+      requestDatas
+    )
+  }
+
+  authUser(requestDatas: AuthRequest): Observable<AuthRequest> {
+    return this.http.post<AuthRequest>(
+      `${this.API_URL}/session`,
       requestDatas
     )
   }
