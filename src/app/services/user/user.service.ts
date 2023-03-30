79e640afd10f52d9675c6146a4efd47ac6143c64
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-29 21:42:44 -0300

    Implement isLoggedIn method

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { UserRequest } from '../../interfaces/User/UserRequest';
 import { AuthRequest } from '../../interfaces/User/auth/AuthRequest';
 import { AuthResponse } from 'src/app/interfaces/User/auth/AuthResponse';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class UserService {
 
   private API_URL = 'http://localhost:3333/v1'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie: CookieService) { }
 
   signUpUser(requestDatas: UserRequest): Observable<UserRequest> {
     return this.http.post<UserRequest>(
@@ -26,5 +27,11 @@ export class UserService {
       `${this.API_URL}/session`,
       requestDatas
     )
+  }
+
+  isLoggedIn(): boolean {
+    const JWT_TOKEN = this.cookie.get('USER_INFO')
+    console.log('token do usuário', JWT_TOKEN);
+    return JWT_TOKEN ? true : false;
   }
 }
