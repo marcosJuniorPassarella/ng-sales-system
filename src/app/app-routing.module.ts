61455b89539044434eaba789feb63bb2aa6ffad6
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-29 21:57:05 -0300

    Added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { AuthGuard } from './guards/auth-guard.service';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AuthGuard]
   }
 ];
 
---
+++ src/app/modules/dashboard/dashboard.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+
+export const ROUTES: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    }
+]
