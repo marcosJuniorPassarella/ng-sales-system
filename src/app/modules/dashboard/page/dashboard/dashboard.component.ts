da7e41fc182fcfdb9780557b9932fdbe40524d09
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-02 16:29:38 -0300

    Added new layout changes

--- src/app/modules/dashboard/page/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/page/dashboard/dashboard.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { UserService } from '../../../../services/user/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: []
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  userName!: string;
+
+  constructor(
+    private cookie: CookieService,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.getUserName();
+  }
+  
+  getUserName(): void {
+    this.userService.getUserInfo()
+      .subscribe({
+        next: (response) => {
+          response && (this.userName = response.name)
+        }
+      })
+  }
 
   logoutUser(): void {
-    alert('Logout Realizado')
+    this.cookie.delete('USER_INFO');
+    void this.router.navigate(['/home']);
   }
+
 }
