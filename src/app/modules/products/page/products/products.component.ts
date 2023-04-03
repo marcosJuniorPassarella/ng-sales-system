2973d9eb03be4f498437029a235cf47d1de1cef2
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-03 20:52:43 -0300

    Implement products table

--- src/app/modules/products/page/products/products.component.ts
+++ src/app/modules/products/page/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllProducts } from 'src/app/interfaces/Products/AllProducts';
+import { ProductsDataTransferService } from 'src/app/shared/services/products-data-transfer.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: []
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productsDatas: Array<AllProducts> = [];
+  productSelected!: AllProducts;
 
+  constructor(private productsDtService: ProductsDataTransferService) { }
+
+  ngOnInit(): void {
+    this.getProductsDatas();
+  }
+
+  getProductsDatas(): void {
+    const allProducts = this.productsDtService.getProductsDatas();
+    allProducts && (this.productsDatas = allProducts);
+    console.log(this.productsDatas);
+  }
 }
