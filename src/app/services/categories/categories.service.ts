75cb0212224e4038abecbcb40f485d5aac3691d9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-03 18:40:02 -0300

    Created categories service and get all categories method

---
+++ src/app/services/categories/categories.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { AllCategories } from 'src/app/interfaces/Categories/AllCategories';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private API_URL = environment.API_URL
+  private JWT_TOKEN = this.cookie.get('USER_INFO');
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.JWT_TOKEN}`
+    }),
+  };
+
+  constructor(private http: HttpClient, private cookie: CookieService) { }
+
+  getAllCategories(): Observable<Array<AllCategories>> {
+    return this.http.get<Array<AllCategories>>(
+      `${this.API_URL}/category/all`, this.httpOptions)
+  }
+
+}
