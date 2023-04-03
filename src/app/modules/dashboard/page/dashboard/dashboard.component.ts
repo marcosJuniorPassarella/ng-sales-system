cdc96c2d09e7eda99c363767d02b78a1483f6e37
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-03 09:15:09 -0300

    Implement toast error

--- src/app/modules/dashboard/page/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/page/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../services/user/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,18 +15,28 @@ export class DashboardComponent implements OnInit {
   constructor(
     private cookie: CookieService,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private messageService: MessageService
   ) { }
 
   ngOnInit(): void {
     this.getUserName();
   }
-  
+
   getUserName(): void {
     this.userService.getUserInfo()
       .subscribe({
         next: (response) => {
           response && (this.userName = response.name)
+        },
+        error: (error) => {
+          console.log(error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar dados do usuário',
+            life: 2000
+          })
         }
       })
   }
