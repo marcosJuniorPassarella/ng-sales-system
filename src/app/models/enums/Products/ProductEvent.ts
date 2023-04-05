5f9be40f669046c3eb671dfe1765fa3af7c7b362
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-05 20:16:46 -0300

    Implement adjust in import paths

---
+++ src/app/models/enums/Products/ProductEvent.ts
@@ -0,0 +1,6 @@
+export enum ProductEvent {
+  ADD_PRODUCT_TITLE = 'Adicionar Produto',
+  EDIT_PRODUCT_TITLE = 'Editar Produto',
+  ADD_PRODUCT_EVENT = 'Adicionar',
+  EDIT_PRODUCT_EVENT = 'Editar',
+}
