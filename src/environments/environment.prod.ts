f5afec585d937491df50718bd0e6f30f067b0df3
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-17 20:21:07 -0300

    Update enviroment folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'http://localhost:3333/v1'
+    API_URL: 'http://localhost:3333/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:3333/v1'
+    API_URL: 'http://localhost:3333/'
 };
