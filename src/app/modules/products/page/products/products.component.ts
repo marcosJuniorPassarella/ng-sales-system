6bcca2b2e67858d65ef6233b17dea0cd43e72acc
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-18 19:47:06 -0300

    Implement new changes in the products page

--- src/app/modules/products/page/products/products.component.ts
+++ src/app/modules/products/page/products/products.component.ts
@@ -1,53 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { MessageService } from 'primeng/api';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { AllProducts } from 'src/app/models/interfaces/Products/AllProducts';
-import { ProductsDataTransferService } from 'src/app/shared/services/products-data-transfer.service';
+
 import { ProductFormComponent } from '../../components/product-form/product-form.component';
 import { ProductEvent } from 'src/app/models/enums/Products/ProductEvent';
+import { GetAllProductsRequest } from 'src/app/models/interfaces/Products/request/GetAllProductsRequest';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products-data-transfer.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: [],
 })
-export class ProductsComponent implements OnInit {
-  productsDatas: Array<AllProducts> = [];
-  productSelected!: AllProducts;
+export class ProductsComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  private ref!: DynamicDialogRef;
+  productsDatas: Array<GetAllProductsRequest> = [];
+  productSelected!: GetAllProductsRequest;
 
-  addProductEvent = {
-    title: ProductEvent.ADD_PRODUCT_TITLE,
-    action: ProductEvent.ADD_PRODUCT_EVENT,
-  };
-  editProductEvent = {
-    title: ProductEvent.EDIT_PRODUCT_TITLE,
-    action: ProductEvent.EDIT_PRODUCT_EVENT,
-  };
+  addProductAction = ProductEvent.ADD_PRODUCT_ACTION;
+  editProductAction = ProductEvent.EDIT_PRODUCT_ACTION;
 
   constructor(
-    private productsDtService: ProductsDataTransferService,
-    private dialogService: DialogService
+    private productsService: ProductsService,
+    private productDataTransferService: ProductsDataTransferService,
+    private dialogService: DialogService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
-    this.getProductsDatas();
+    this.getServiceProductsDatas();
   }
 
-  getProductsDatas(): void {
-    const allProducts = this.productsDtService.getProductsDatas();
-    allProducts && (this.productsDatas = allProducts);
-    console.log(this.productsDatas);
+  getServiceProductsDatas(): void {
+    const productsLoaded = this.productDataTransferService.getProductsDatas();
+    if (productsLoaded) {
+      this.productsDatas = productsLoaded;
+    }
   }
 
-  handleProduct(productEvent: { title: string; action: string }): void {
-    this.dialogService.open(ProductFormComponent, {
-      header: productEvent.title,
+  getAPIProductsDatas(): void {
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 3000,
+          });
+        },
+      });
+  }
+
+  handleProduct(action: string, id?: string): void {
+    this.ref = this.dialogService.open(ProductFormComponent, {
+      header: action,
       width: '70%',
       contentStyle: { overflow: 'auto' },
       baseZIndex: 10000,
       maximizable: true,
       data: {
-        action: productEvent.action,
+        event: { action, id },
       },
     });
+
+    this.ref.onClose.pipe(takeUntil(this.destroy$)).subscribe({
+      next: () => this.getAPIProductsDatas(),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
