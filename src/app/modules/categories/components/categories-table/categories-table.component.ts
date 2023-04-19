fce0cfe1ae9810b9f8dc71036fed55c644a3e358
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-19 19:00:43 -0300

    Implement categories table class

--- src/app/modules/categories/components/categories-table/categories-table.component.ts
+++ src/app/modules/categories/components/categories-table/categories-table.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CategoryEvent } from 'src/app/models/enums/Categories/CategoryEvent';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/Categories/GetCategoriesResponse';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/Categories/event/DeleteCategoryAction';
+import { EventAction } from 'src/app/models/interfaces/event/EventAction';
 
 @Component({
   selector: 'app-categories-table',
   templateUrl: './categories-table.component.html',
   styleUrls: [],
 })
-export class CategoriesTableComponent {}
+export class CategoriesTableComponent {
+  @Input() categories: Array<GetCategoriesResponse> = [];
+  @Output() categoryEvent = new EventEmitter<EventAction>();
+  @Output() deleteCategoryEvent = new EventEmitter<DeleteCategoryAction>();
+
+  categorySelected!: GetCategoriesResponse;
+  addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
+  editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
+
+  handleCategoryEvent(action: string, id?: string): void {
+    if (action && action !== '') {
+      const categoryEventData = id && id !== '' ? { action, id } : { action };
+      this.categoryEvent.emit(categoryEventData);
+    }
+  }
+
+  handleDeleteCategoryEvent(category_id: string, categoryName: string): void {
+    if (category_id !== '' && categoryName !== '') {
+      ~this.deleteCategoryEvent.emit({ category_id, categoryName });
+    }
+  }
+}
