e5b2227a20e5bbf626c8a7c559f98a0e30f2ad31
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-19 17:56:27 -0300

    Added new updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,14 @@ import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'ng-sales-system';
 
-  constructor(private primengConfig: PrimeNGConfig) { }
+  constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
     this.primengConfig.ripple = true;
-    this.primengConfig.setTranslation({
-      matchAll: 'Corresponder a todos',
-      matchAny: 'Corresponder a qualquer',
-      apply: 'Aplicar',
-      clear: 'Limpar',
-      cancel: 'Cancelar',
-      removeRule: 'Remover regra',
-      addRule: 'Adicionar regra',
-      startsWith: 'Começa com'
-    })
   }
 }
